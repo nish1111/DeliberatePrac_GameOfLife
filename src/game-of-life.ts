234ae619e670ba2056f6c8896ca3136760ea8485
Sylvain Pillay <sylvain.pillay@chillisoft.co.za>
2022-07-22 16:19:00 +0200

    refactor abit

--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -1,17 +1,14 @@
 export class GameOfLife {
 
     play(grid: number[][]) {
-        const initialRow = grid[0]
+        const initialRow = grid
 
-        for (let index = 0; index < initialRow.length; index++) {
-            if (initialRow[index] === 1) {
-                const previous = initialRow[0]
-                const next = initialRow[2]
-                if (previous === 0 && next === 0) {
-                    return [[0, 0, 0]]
-                }
-            }
+
+        const previous = initialRow[0][0]
+        const next = initialRow[0][2]
+        if (previous === 0 && next === 0) {
+            return [[0, 0, 0]]
         }
-        return [initialRow]
+        return initialRow
     }
 }
